da32d79a36ba80342eeadb612fd037677752d462
Lucas Battestin <l.battestin@reply.com>
2026-02-07 16:40:19 -0300

    Initial admin web

--- config.local.js
+++
@@ -1,4 +0,0 @@
-window.ORE_ADMIN_DEFAULTS = {
-  SUPABASE_URL: "https://douyjoogmnwjmfaudjke.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvdXlqb29nbW53am1mYXVkamtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MTk2MzgsImV4cCI6MjA4MTI5NTYzOH0.ryElWyHw424gZMOUH9C6MFEHKHWKw_xzlwod6xwZ_G0"
-};
